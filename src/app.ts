6091c3c3c3be15dd744ca8ff154b4435296d73b4
AkibKST <akibkst22@gmail.com>
2025-04-20 11:58:02 +0600

    User & Admin Creation with transaction

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ import { UserRoutes } from "./app/modules/User/user.route";
 const app: Application = express();
 app.use(cors());
 
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
   res.send({
     message: "PH Healthcare server...",
--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,9 +1,30 @@
-import { create } from "domain";
+import { PrismaClient, UserRole } from "../../../../generated/prisma";
 
-const createAdmin = async () => {
-  return {
-    message: "Admin created successfully",
+const prisma = new PrismaClient();
+
+// Function to create an admin user
+const createAdmin = async (data: any) => {
+  const userData = {
+    email: data.admin.email,
+    password: data.password,
+    role: UserRole.ADMIN,
   };
+
+  //create user and admin in a transaction
+  const result = await prisma.$transaction(async (transactionClient) => {
+    const createdUserData = await transactionClient.user.create({
+      data: userData,
+    });
+
+    const createdAdminData = await transactionClient.admin.create({
+      data: data.admin,
+    });
+
+    return console.log(createdAdminData);
+  });
+  //-------------------------------------------------
+
+  return result;
 };
 
 export const UserServices = {
