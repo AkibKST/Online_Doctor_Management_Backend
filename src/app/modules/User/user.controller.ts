0979ca5042b38ad8dd2441e56887f8cdb1bb2320
AkibKST <akibkst22@gmail.com>
2025-04-20 12:40:49 +0600

    Create try catch exception handle and send a valid response utils

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from "express";
 import { UserServices } from "./user.service";
+import catchAsync from "../../../sharedUtils/catchAsync";
+import sendResponse from "../../../sharedUtils/sendResponse";
+import httpStatus from "http-status";
 
-const createAdmin = async (req: Request, res: Response) => {
+const createAdmin = catchAsync(async (req: Request, res: Response) => {
   const result = await UserServices.createAdmin(req.body);
-  res.send(result);
-};
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Admin Created successfully!",
+    data: result,
+  });
+});
 
 export const UserControllers = {
   createAdmin,
