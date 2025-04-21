6d824c610371f9ddda59e719f434d6eeefb521ae
AkibKST <akibkst22@gmail.com>
2025-04-21 09:28:44 +0600

    Selecting Valid Data Fields Using the pick Function

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -3,10 +3,13 @@ import catchAsync from "../../../sharedUtils/catchAsync";
 import { AdminServices } from "./admin.service";
 import sendResponse from "../../../sharedUtils/sendResponse";
 import httpStatus from "http-status";
+import pick from "../../../sharedUtils/pick";
 
 //get all admin data from DB
 const getAllFromDB: RequestHandler = catchAsync(
   async (req: Request, res: Response) => {
+    //Selecting Valid Data Fields Using the pick Function
+    pick(req.query, ["name", "email", "searchTerm", "contactNumber"]);
     // console.log(req.query);
 
     const result = await AdminServices.getAllFromDB(req.query);
