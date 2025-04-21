a36fac704f9abbab0ef730e6dfc80b5dc5e9d362
AkibKST <akibkst22@gmail.com>
2025-04-21 09:07:01 +0600

    Implementing Filtering on Specific Fields and Values

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -3,28 +3,37 @@ import { Prisma, PrismaClient } from "../../../../generated/prisma";
 const prisma = new PrismaClient();
 
 const getAllFromDB = async (params: any) => {
+  //if searchTerm is present in query params, then add it to the where condition
+  const { searchTerm, ...filterData } = params;
   const andCondition: Prisma.AdminWhereInput[] = [];
+  const adminSearchAbleFields = ["name", "email"];
 
+  // console.log(filterData, "filterData");
   if (params.searchTerm) {
     andCondition.push({
-      OR: [
-        {
-          name: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
+      OR: adminSearchAbleFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
         },
-        {
-          email: {
-            contains: params.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
+      })),
     });
   }
 
+  //Implementing Filtering on Specific Fields and Values
+  if (Object.keys(filterData).length > 0) {
+    andCondition.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+        },
+      })),
+    });
+  }
+
+  //genarate where condition{} from andCondition[]
   const whereCondition: Prisma.AdminWhereInput = { AND: andCondition };
+
   const result = await prisma.admin.findMany({
     //where searchTerm contains in name or email
     where: whereCondition,
