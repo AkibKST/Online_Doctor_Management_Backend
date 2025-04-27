648c3973b717a4beea1ad5d31a3ab99c8d782bec
AkibKST <akibkst22@gmail.com>
2025-04-27 13:41:40 +0600

    Implementing Authentication Middleware for Role-Based Access on APIs

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
 import { UserControllers } from "./user.controller";
+import auth from "../../middleware/auth";
 
 const router = express.Router();
 
-router.post("/", UserControllers.createAdmin);
+router.post("/", auth("ADMIN", "SUPER_ADMIN"), UserControllers.createAdmin);
 
 export const UserRoutes = router;
